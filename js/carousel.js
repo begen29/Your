bafcb8455fe9ce4d1ff9c4145584a767aa2ec85a
begen29 <begentanya@ukr.net>
2021-02-21 21:07:19 +0000

    carus

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+const images = [
+    'img/men-leaft.jpg',
+    'img/friends.jpg',
+    'img/water.jpg',
+    'img/flowers.jpg',
+    'img/gress-del.jpg'
+]
+
+let currIdx = 0;
+
+function showCurrentImage() {
+    const imgContainer = document.querySelector('.current');
+    imgContainer.src = images[currIdx];
+
+}
+showCurrentImage();
+
+function nextImage() {
+    currIdx++;
+    if (currIdx >= images.length) currIdx = 0;
+    showCurrentImage();
+}
+
+function prevImage() {
+    currIdx--;
+    if (currIdx < 0) currIdx = images.length - 1;
+    showCurrentImage();
+}
+
+document.querySelectorAll('.prev').addEventListener('click', pverImage);
+document.querySelector('.next').addEventListener('click', nextImage);
